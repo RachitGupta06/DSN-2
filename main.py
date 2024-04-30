88ff7b663f614c32a4b300cf60a3250dfd2fd5f8
nirmal <nirmalanand15.9.2003@gmail.com>
2024-04-30 12:13:02 +0530

    fix: remove repeating routess

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from mongo_functions import *
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '825'
-is_loggedin = False
 
 @app.route('/')
 def index():
@@ -17,7 +17,6 @@ def index():
             return render_template('index.html', is_loggedin=False, button_text="Login | Sign In")
 
     return render_template('index.html', is_loggedin=False, button_text="Login | Sign In")
-    # return redirect(url_for('index'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -46,8 +45,6 @@ def signup():
         email = request.form.get('email').strip()
         password = request.form.get('password')
         
-        # print(colored(f"{full_name, username, email, password}", "green"))
-
         if add_user(
             full_name,
             username,
@@ -59,6 +56,21 @@ def signup():
             return jsonify({"message": "failed to add the user"})
 
     return render_template('signup.html')
+
+@app.route('/submit_form', methods=['POST'])
+def submit_form():
+    if request.method == 'POST':
+        car_name = request.form['carName']
+        contact_info = request.form['contactInfo']
+        max_passengers = request.form['maxPassengers']
+        seat_location = request.form['seatLocation']
+        location = request.form['location']
+        if location == 'Other':
+            location = request.form['otherLocation']
+        from_date = request.form['fromDate']
+        to_date = request.form['toDate']
+        
+        return 'Form submitted successfully!'
 
 @app.route('/logout')
 def logout():
@@ -82,33 +94,6 @@ def bookride():
 def contact():
     return render_template('contactUs.html')
 
-from flask import Flask, request, render_template
 
-app = Flask(_name_)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/submit_form', methods=['POST'])
-def submit_form():
-    if request.method == 'POST':
-        car_name = request.form['carName']
-        contact_info = request.form['contactInfo']
-        max_passengers = request.form['maxPassengers']
-        seat_location = request.form['seatLocation']
-        location = request.form['location']
-        if location == 'Other':
-            location = request.form['otherLocation']
-        from_date = request.form['fromDate']
-        to_date = request.form['toDate']
-        
-        # Here you can process the form data (e.g., store it in a database, send an email, etc.)
-        
-        return 'Form submitted successfully!'
-
-if _name_ == '_main_':
-    app.run(debug=True)
-
-if __name__ == '__main__':
+if __name__ == '_main_':
     app.run()
